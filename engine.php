29fb3928ecacd64a0f2a94b975bd25d77f9e34a8
Richard Obiri <obiriaframson@gmail.com>
2020-11-24 17:40:50 +0000

    guides

--- engine.php
+++ engine.php
@@ -4,10 +4,17 @@
 
 function AliceRequest()
 {
+  // allow database access
 
 
+  // make a fetch request usiing MYSQL to get a response 
+
+
+  // loop through your respose ( foreach or while  )
+
+
+  // echo your response ...
     
-
 }
 
 
